fbe1d857325e2c12d92f0e882cbbe402569e8a5e
SebastianCordova473 <sebas-12359@outlook.es>
2022-08-12 15:23:43 -0500

    fin minValue in arrays of integers

--- com/streams/StreamsMinMaxValue.java
+++ com/streams/StreamsMinMaxValue.java
@@ -13,10 +13,24 @@ public class StreamsMinMaxValue {
   static Function<List<Integer>, Optional<Integer>> findMaxValue = integerList -> integerList.stream().reduce(
       (x, y) -> x > y ? x : y);
 
-  public static void main(String[] args) {
+  static Function<List<Integer>, Optional<Integer>> findMinValue = integers -> integers.stream().reduce((x,y) -> x<y ? x : y);
 
+  public static void main(String[] args) {
     List<Integer> integerList = Arrays.asList(6, 7, 8, 9, 10);
-    Optional<Integer> maxValue = findMaxValue.apply(integerList);
+
+    Optional<Integer> minValue =  findMinValue.apply(integerList);
+
+    System.out.println("minValue: " + minValue.orElse(0));
+
+ /*   Optional<Integer> maxValue = findMaxValue.apply(integerList);
     System.out.println("maxValue is: " + maxValue.orElse(0));
+    Optional<Integer> maxValueOption = findMaxValue.apply(integerList);
+    if (maxValueOption.isPresent()){
+      System.out.println("Max Value is using optional: " + maxValueOption);
+    } else {
+      System.out.println("Max Value is notFound");
+    }*/
+
   }
 }
+
